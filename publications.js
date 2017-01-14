27b78afabc0edc7ee74f53a16ff8daf2dde6b0b9
weihc06 <weihongchuan@gmail.com>
2017-01-14 13:58:15 -0600

    lala

--- publications.js
+++ publications.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $("#a_nav_jenny").css({
+$(document).ready( function() {
+    $("#a_nav_publications").css({
 	"background-color": NAV_BG_COLOR_SELECTED,
 	"color": "white"
     });
