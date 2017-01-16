e7b8ab718eab9593d2592a208edb87a3257322f6
weihc06 <weihongchuan@gmail.com>
2017-01-15 21:41:16 -0600

    bibtex button working

--- publications.js
+++ publications.js
@@ -1,6 +1,21 @@
 $(document).ready( function() {
+    var iPaper = 1;
+    var btnSelector, divSelector;
     $("#a_nav_publications").css({
 	"background-color": NAV_BG_COLOR_SELECTED,
 	"color": "white"
     });
+
+    /*$("#btn_bibtex1").click( function() {
+	$("#div_bibtex1").toggle();
+    });*/
+    
+    for(iPaper = 1; iPaper<=1; iPaper++) {
+	btnSelector = "#btn_bibtex" + iPaper;
+	divSelector = "#div_bibtex" + iPaper;
+	$( btnSelector ).click( function() {
+	    $(divSelector).toggle();
+	})
+    }
+
 });
