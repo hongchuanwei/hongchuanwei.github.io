81f4ee73a3dde56ad6ce79302a2a812f5bd812eb
weihc06 <weihongchuan@gmail.com>
2017-02-15 22:48:54 -0600

    hello world

--- index.js
+++ index.js
@@ -7,4 +7,24 @@ $(document).ready(function(){
 	"background-color": NAV_BG_COLOR_SELECTED,
 	"color": "white"
     });
+
+    toggleCursor(0);
+
+    function toggleCursor(isToggled) {
+	var bgColor, ftColor;
+	if(isToggled===0) {
+	    bgColor = "#e52662";
+	    ftColor = "white";
+	    isToggled = 1;
+	} else {
+	    bgColor = "white";
+	    ftColor = "#e52662";
+	    isToggled = 0;
+	}
+	$("#div-cursor").css({
+	    "background-color": bgColor,
+	    "color": ftColor,
+	});
+	setTimeout(toggleCursor, 1500, isToggled);
+    };
 });
