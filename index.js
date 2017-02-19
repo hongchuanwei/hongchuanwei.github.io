357c477024d46a0a061e811a7134a82a7b19d584
Hongchuan Wei <hwei@epic.com>
2017-02-18 21:24:54 -0600

    home appears to be broken, commit to check on another computer

--- index.js
+++ index.js
@@ -5,42 +5,42 @@ var colors = ["red", "green", "blue","yellow"];
 $(document).ready(function(){  
 
     $("#a_nav_header").css({
-	"background-color": NAV_BG_COLOR_SELECTED,
-	"color": "white"
+		"background-color": NAV_BG_COLOR_SELECTED,
+		"color": "white"
     });
 
     // The blinking cursor
     toggleCursor(0);
 
     function toggleCursor(isToggled) {
-	var bgColor, ftColor;
-	if(isToggled===0) {
-	    bgColor = "#e52662";
-	    ftColor = "white";
-	    isToggled = 1;
-	} else {
-	    bgColor = "white";
-	    ftColor = "#e52662";
-	    isToggled = 0;
-	}
-	$("#div-cursor").css({
-	    "background-color": bgColor,
-	    "color": ftColor,
-	});
-	setTimeout(toggleCursor, 1500, isToggled);
+		var bgColor, ftColor;
+		if(isToggled===0) {
+			bgColor = "#e52662";
+			ftColor = "white";
+			isToggled = 1;
+		} else {
+			bgColor = "white";
+			ftColor = "#e52662";
+			isToggled = 0;
+		}
+		$("#div-cursor").css({
+			"background-color": bgColor,
+			"color": ftColor,
+		});
+		setTimeout(toggleCursor, 1500, isToggled);
     };
 
     // The flipping letters
-    if (!String.format) {
-	String.format = function(format) {
-	    var args = Array.prototype.slice.call(arguments, 1);
-	    return format.replace(/{(\d+)}/g, function(match, number) { 
-		return typeof args[number] != 'undefined'
-		    ? args[number] 
-		    : match
-		;
-	    });
-	};
+    /*if (!String.format) {
+		String.format = function(format) {
+			var args = Array.prototype.slice.call(arguments, 1);
+			return format.replace(/{(\d+)}/g, function(match, number) { 
+			return typeof args[number] != 'undefined'
+				? args[number] 
+				: match
+			;
+			});
+		};
     }
 
     
@@ -48,21 +48,44 @@ $(document).ready(function(){
     var textForDisplay = "This is my website";
     var i;
     for(i of textForDisplay) {
-	$("#p-colorful").append(String.format('<span class="span-color">{0}</span>', i));
+		$("#p-colorful").append(String.format('<span class="span-color">{0}</span>', i));
     }
 
     var color="red";
    
     $(".span-color").each(function() {
-	$(this).mouseenter(function(){
-	    $(this).css({
-		"background-color": colors[Math.floor((Math.random() * 4))],
-		"display": "inline-block",
-		"position": "relative",
-		"left": "10px",
-		"top": "20px"
-	    });
-	});
-    });
+		$(this).mouseenter(function(){
+			var offsets = $(this).offset();
+			$(this).css({
+				"background-color": colors[Math.floor((Math.random() * 4))],
+				"display": "inline-block",
+				"position": "relative",
+				"left": "10px",
+				"top": "20px"
+			});
+		});
+    });*/
+	
+	var myName = "This is my website";
+
+	var red = [0, 100, 63];
+	var orange = [40, 100, 60];
+	var green = [75, 100, 40];
+	var blue = [196, 77, 55];
+	var purple = [280, 50, 60];
+	var letterColors = [red, orange, green, blue, purple];
+
+	drawName(myName, letterColors);
+
+	if(10 < 3)
+	{
+		bubbleShape = 'square';
+	}
+	else
+	{
+		bubbleShape = 'circle';
+	}
+
+	bounceBubbles();
 
 });
