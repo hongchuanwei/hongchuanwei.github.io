2d9c18a7fa4c7ff1ad1623d0b2142a19dc760d34
river <weihc06@gmail.com>
2017-07-09 00:39:38 -0400

    updated fun page on document ready

--- fun.js
+++ fun.js
@@ -1,20 +1,38 @@
+// Constants
+NUM_GAMES = 2;
 
-// window.gameIdx = 0; // index of game to be shown
 
-$(document).ready(function() {
-	
+
+$(function() {
+
+	// properties 
 	this.gameIdx = 0;
+	this.containerDiv = $("#div-container");
+	this.carouselLeft = $("#carousel-l");
+	this.carouselRight = $("#carousel-r");
+
+	// callback functions 
+
+	//this.initialize();
 	
-	$("#div-middle-top").html(this.gameIdx); 
-	
-	$("#carousel-l").click(leftArrowClicked.bind(this,-1));
-	$("#carousel-r").click(leftArrowClicked.bind(this,1));
-	
-	function leftArrowClicked(increment) {
+	 
+
+	this.containerDiv.html( this.gameIdx );	
+
+	this.carouselLeft.click(arrowClicked.bind(this,-1));
+	this.carouselRight.click(arrowClicked.bind(this,1)); 
+
+
+	function initialize() {
+	}
+
+	function arrowClicked(increment) {
 		this.gameIdx += increment;
-		$("#div-middle-top").html(this.gameIdx); 
+		this.gameIdx %= NUM_GAMES;
+		this.containerDiv.html( this.gameIdx );
 	}
 
 });
+
 
 
