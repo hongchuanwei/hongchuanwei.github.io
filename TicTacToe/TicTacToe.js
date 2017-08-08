0520b64104f952f470c15eb8be0035ac7c4eddfb
river <weihc06@gmail.com>
2017-08-08 00:15:28 -0400

    button logic improved

--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -3,69 +3,57 @@
 $(new function() {
 
 	/******* Constants *******/
-	/**
-	 * Player's piece.
-	 */
+	// Player's piece.
 	const PLAYER_PIECE = Piece.X;
-	/**
-	 * AI's piece.
-	 */
+	// AI's piece.
 	const AI_PIECE = Piece.O;
 
 	/******* Properties *******/
-	/**
-	 * If the game has started
-	 */
+	// If the game has started
 	this.__isGameStarted = false;
-	/**
-	 * Who plays first. Default is player
-	 */
+	// If canvas has been clicked and is handling stuff
+	this.__isBoardClicked = false;
+	// Who plays first. Default is player
 	this.__firstPiece = PLAYER_PIECE;
-	/**
-	 * Model of the game. Default is Player moves first
-	 */
+	// Model of the game. Default is Player moves first
 	this.__model = new TTTModel(this.__firstPiece, PLAYER_PIECE);
-	/**
-	 * View of the game.
-	 */
+	// View of the game.
 	this.__board = new TTTBoard();
-	/**
-	 * AI
-	 */
+	// AI
 	this.__AI = new TTTAI(this.__model);
-	/**
-	 * The container div
-	 */
+	// The container div
 	this.__containerDiv = $("#div-TicTacToe");
-	/**
-     * The restart button
-	 */
+	// The restart button
 	this.__resetDiv = $("#div-restart");
-	/**
-	 * The computer first button
-	 */
-	this.__computerDiv = $("#div-obutton");
+	// The computer first button
+	this.__ObuttonDiv = $("#div-obutton");
+	// The player first button
+	this.__XButtonDiv = $("#div-xbutton");
 
 	/******* Delegatess *******/
-	/**
-	 * Canvas clicking delegate
-	 */
+	// Canvas clicking delegate
 	this.__onCanvasClicked = onCanvasClicked.bind(this);
-	/**
-	 * reset delegate
-	 */
+	// reset delegate
 	this.__onResetClicked = reset.bind(this);
-	/**
-	 * Computer button delegate
-	 */
-	this.__onComputerClicked = setAIFirst.bind(this);
+	// Computer button delegate
+	this.__onOButtonClicked = setAIFirst.bind(this);
+	// Obutton mouseenter delegate
+	this.__onOButtonEnter = onObuttonEnter.bind(this);
+	// Obutton mouseleave delegate
+	this.__onOButtonLeave = onObuttonLeave.bind(this);
 
+	/******* Initialization *******/
 	this.__containerDiv.append( this.__board.canvas );
 	this.__board.canvas.bind("click", this.__onCanvasClicked);
 
 	this.__resetDiv.bind("click", this.__onResetClicked);
 
-	this.__computerDiv.bind("click", this.__onComputerClicked);
+	this.__ObuttonDiv.bind("click", this.__onOButtonClicked);
+	this.__ObuttonDiv.bind("mouseenter", this.__onOButtonEnter);
+	this.__ObuttonDiv.bind("mouseleave", this.__onOButtonLeave);
+
+	this.__XButtonDiv.addClass("div-button-active");
+
 
 	/******* Methods *******/
 	/**
@@ -73,17 +61,24 @@ $(new function() {
 	 */
 	function reset() {
 		this.__isGameStarted = false;
-		if ( this.__firstPiece !== AI_PIECE ) {
+		this.__isBoardClicked = false;
+		if ( this.__firstPiece === AI_PIECE ) {
 			// always assumes player plays first
-			this.__firstPiece = PLAYER_PIECE; 
+			this.__firstPiece = PLAYER_PIECE;
 			this.__model = new TTTModel(this.__firstPiece, PLAYER_PIECE);
 			this.__AI = new TTTAI(this.__model);
 		}
-		
+		this.__ObuttonDiv.bind("click", this.__onOButtonClicked);
+		this.__ObuttonDiv.bind("mouseenter", this.__onOButtonEnter);
+		this.__ObuttonDiv.bind("mouseleave", this.__onOButtonLeave);
+
+		this.__XButtonDiv.addClass("div-button-active");
+		this.__ObuttonDiv.removeClass("div-button-active");
+
 	 	this.__model.reset();
 		this.__board.reset();
 	}
-	
+
 
 	/**
 	 * Handler for the mouse click event
@@ -104,11 +99,24 @@ $(new function() {
 
 		if (boardState !== GameState.CONTINUE) { return; }
 
+		if (this.__isBoardClicked === true) { return; }
+
+		this.__isBoardClicked = true;
+
 		let nextPiece = model.getNextPiece();
 
 		let isPieceSetSuccess = model.setPiece(i, j, nextPiece);
 
 		if (isPieceSetSuccess) {
+			if (!this.__isGameStarted) {
+				this.__isGameStarted = true;
+				this.__ObuttonDiv.unbind("click", this.__onOButtonClicked);
+				this.__ObuttonDiv.unbind("mouseenter", this.__onOButtonEnter);
+				this.__ObuttonDiv.unbind("mouseleave", this.__onOButtonLeave);
+			}
+			this.__XButtonDiv.removeClass("div-button-active");
+			this.__ObuttonDiv.addClass("div-button-active");
+
 			board.drawPiece(i, j, nextPiece);
 
 			boardState = model.getGameState();
@@ -117,42 +125,82 @@ $(new function() {
 				// play game ending movie
 				alert("player wins or draw");
 			} else {
-				let nextPos = AI.bestMove();
-				nextPiece = model.getNextPiece();
-				isPieceSetSuccess = model.setPiece(nextPos.xPos, nextPos.yPos, nextPiece);
-				if (!isPieceSetSuccess) { alert("sth wrong with AI"); }
+				setTimeout(function() {
+					this.__isBoardClicked = true;
+					let nextPos = this.__AI.bestMove();
+					let nextPiece = this.__model.getNextPiece();
+					let isPieceSetSuccess = this.__model.setPiece(nextPos.xPos, nextPos.yPos, nextPiece);
+					if (!isPieceSetSuccess) { alert("sth wrong with AI"); }
 
-				board.drawPiece(nextPos.xPos, nextPos.yPos, nextPiece);
+					this.__board.drawPiece(nextPos.xPos, nextPos.yPos, nextPiece);
 
-				boardState = model.getGameState();
-				if (boardState != GameState.CONTINUE) {
-					// play game ending movie
-					alert("computer wins or draw");
-				}
+					setTimeout(function(){
+						this.__isBoardClicked = true;
+						this.__XButtonDiv.addClass("div-button-active");
+						this.__ObuttonDiv.removeClass("div-button-active");
+						this.__isBoardClicked = false;
+					}.bind(this), 300);
+
+					let boardState = this.__model.getGameState();
+					if (boardState != GameState.CONTINUE) {
+						// play game ending movie
+						alert("computer wins or draw");
+					}
+					this.__isBoardClicked = false;
+				}.bind(this), 300);
 			}
 		}
+
+		this.__isBoardClicked = false;
 	}
 
 	/**
 	 * Handler for the computer button
 	 */
 	function setAIFirst() {
+		// update game related logic
 		if (this.__isGameStarted) { return; }
 		this.__isGameStarted = true;
+		this.__ObuttonDiv.addClass("div-button-active");
+		this.__ObuttonDiv.unbind("click", this.__onOButtonClicked);
+		this.__ObuttonDiv.unbind("mouseenter", this.__onOButtonEnter);
+		this.__ObuttonDiv.unbind("mouseleave", this.__onOButtonLeave);
 
 		// update model and AI
-		this.__firstPiece = AI_PIECE; 
+		this.__firstPiece = AI_PIECE;
 		this.__model = new TTTModel(this.__firstPiece, PLAYER_PIECE);
 		this.__AI = new TTTAI(this.__model);
 
 		// make the first move
 		let nextPos = this.__AI.bestMove(AIAlgorithm.RANDOM);
 		let nextPiece = this.__model.getNextPiece();
-		
+
 		let isPieceSetSuccess = this.__model.setPiece(nextPos.xPos, nextPos.yPos, nextPiece);
 		if (!isPieceSetSuccess) { alert("sth wrong with AI"); }
 
 		this.__board.drawPiece(nextPos.xPos, nextPos.yPos, nextPiece);
+
+		setTimeout(function(){
+			this.__isBoardClicked = true;
+			this.__XButtonDiv.addClass("div-button-active");
+			this.__ObuttonDiv.removeClass("div-button-active");
+			this.__isBoardClicked = false;
+		}.bind(this), 300);
+	}
+
+	/**
+	 * Handler for Obutton mouseenter effect
+	 */
+	function onObuttonEnter() {
+		this.__XButtonDiv.removeClass("div-button-active");
+		this.__ObuttonDiv.addClass("div-button-active");
+	}
+	/**
+	 * Handler for Obutton mouseleave effect
+	 */
+	function onObuttonLeave() {
+		this.__XButtonDiv.addClass("div-button-active");
+		this.__ObuttonDiv.removeClass("div-button-active");
 	}
 
 });
