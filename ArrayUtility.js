e38ee14c86a5a8329e8b03bea4110092ee5d2942
river <weihc06@gmail.com>
2017-08-24 21:40:54 -0400

    win lose logic

--- ArrayUtility.js
+++ ArrayUtility.js
@@ -3,7 +3,9 @@ ArrayUtility = function core$ArrayUtility() {
 }
 
 ArrayUtility.prototype = {
-
+	/**
+	 * Creates (multiple-dimensional) array.
+	 */
 	createArray: function createArray(length) {
         var arr = new Array(length || 0),
             i = length;
@@ -14,5 +16,40 @@ ArrayUtility.prototype = {
         }
 
         return arr;
-    }
+    },
+
+	/**
+     * Helper function that finds one array in another array. Maybe change to KMP
+	 * @param {Array} arrShort - the shorter array
+	 * @param {Array} arrLong - the longer array
+	 * @return {boolean} true if arrShort is in arrLong
+     */
+    isArrayInArray: function fun$Gomoku$isArrayInArray(arrShort, arrLong) {
+        var lCount = arrLong.length;
+        var sCount = arrShort.length;
+        var k;
+        for (var i = 0; i <= lCount - sCount; i++) {
+            k = 0;
+            for (var j = 0; j < sCount; j++) {
+                if (arrLong[i + j] == arrShort[j]) {
+					k++;
+				} else {
+					break;
+				}
+            }
+            if (k == sCount) return true;
+        }
+        return false;
+    },
+
+	/**
+     * Helper function that finds if any array in the list of array is in another array
+	 * @return {Boolean} True if any array in arrList in in arr
+     */
+    isAnyArraysInArray: function fun$Gomoku$isInArrays(arrList, arrLong) {
+        for (var i = 0; i < arrList.length; i++) {
+            if (this.isArrayInArray(arrList[i], arrLong)) { return true; }
+        }
+        return false;
+    },
 }
